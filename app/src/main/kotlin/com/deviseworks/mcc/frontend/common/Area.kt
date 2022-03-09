991c731d330b9a8e7b01a5b947d25faefaf82a67
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-10 00:58:41 +0900

    共通プロパティを定義

---
+++ app/src/main/kotlin/com/deviseworks/mcc/frontend/common/Area.kt
@@ -0,0 +1,9 @@
+package com.deviseworks.mcc.frontend.common
+
+import csstype.ident
+
+object Area {
+    val Appbar = ident("appbar")
+    val Drawer = ident("drawer")
+    val Content = ident("content")
+}
---
+++ app/src/main/kotlin/com/deviseworks/mcc/frontend/common/Sizes.kt
@@ -0,0 +1,12 @@
+package com.deviseworks.mcc.frontend.common
+
+import csstype.px
+
+object Sizes {
+    object Appbar{
+        val Height = 64.px
+    }
+    object Drawer{
+        val Width = 240.px
+    }
+}
