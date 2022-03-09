08c7774e44700992228dc5c21e557fa8e5c75fe8
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-10 00:59:59 +0900

    表示方法を変更

--- app/src/main/kotlin/com/deviseworks/mcc/frontend/App.kt
+++ app/src/main/kotlin/com/deviseworks/mcc/frontend/App.kt
@@ -1,10 +1,22 @@
 package com.deviseworks.mcc.frontend
 
+import com.deviseworks.mcc.frontend.common.Area
+import com.deviseworks.mcc.frontend.common.Sizes
+import com.deviseworks.mcc.frontend.component.Appbar
+import com.deviseworks.mcc.frontend.component.DrawerPermanent
+import com.deviseworks.mcc.frontend.component.SampleTheme
+import csstype.Auto
+import csstype.Display
+import csstype.GridTemplateAreas
+import csstype.array
 import kotlinx.browser.document
+import mui.material.Box
+import mui.material.CssBaseline
+import mui.system.ThemeProvider
+import mui.system.sx
 import react.FC
 import react.Props
 import react.create
-import react.dom.html.ReactHTML.h1
 import react.dom.render
 
 fun main(){
@@ -15,7 +27,31 @@ fun main(){
 }
 
 private val App = FC<Props>{
-    h1{
-        +"Hello World"
+    ThemeProvider{
+        this.theme=SampleTheme
+        CssBaseline()
+
+        Box{
+            sx{
+                display=Display.grid
+
+                gridTemplateRows = array(
+                    Sizes.Appbar.Height,
+                    Auto.auto
+                )
+
+                gridTemplateColumns = array(
+                    Sizes.Drawer.Width,
+                    Auto.auto
+                )
+
+                gridTemplateAreas = GridTemplateAreas(
+                    arrayOf(Area.Appbar, Area.Appbar),
+                    arrayOf(Area.Drawer, Area.Content)
+                )
+            }
+            Appbar()
+            DrawerPermanent()
+        }
     }
 }
