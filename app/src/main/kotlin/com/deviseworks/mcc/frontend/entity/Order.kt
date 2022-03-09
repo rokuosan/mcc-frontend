2ccabf0871374a38edc4eac9291aa0483f596a76
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-09 14:41:17 +0900

    オーダエンティティを追加

---
+++ app/src/main/kotlin/com/deviseworks/mcc/frontend/entity/Order.kt
@@ -0,0 +1,45 @@
+package com.deviseworks.mcc.frontend.entity
+
+import kotlinx.serialization.SerialName
+
+@kotlinx.serialization.Serializable
+data class Order(
+    // ID
+    @SerialName("id")
+    val id: Int? = null,
+
+    // 送信主のUUID
+    @SerialName("sender_uuid")
+    val senderUUID: String? = null,
+
+    // 送信日付
+    @SerialName("date")
+    val date: String? = null,
+
+    // 送信時刻
+    @SerialName("time")
+    val time: String? = null,
+
+    // コマンドクエリ
+    @SerialName("command")
+    val command: String? = null,
+
+    // 完了ステータス
+    @SerialName("is_done")
+    var isDone: Boolean? = null,
+
+    // キャンセルステータス
+    @SerialName("is_canceled")
+    var isCanceled: Boolean? = null
+)
+
+
+/* 想定データ形式
+id,
+sender,
+date, 形式: yyyy-MM-dd
+time, 形式: hh:mm:ss
+command,
+isDone,
+isCanceled
+ */
