a7053bd45310fcda7a3b0ec52328c64d0f57a870
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-10 03:57:52 +0900

    フォントの追加

--- app/src/main/kotlin/com/deviseworks/mcc/frontend/common/Themes.kt
+++ app/src/main/kotlin/com/deviseworks/mcc/frontend/common/Themes.kt
@@ -3,7 +3,7 @@ package com.deviseworks.mcc.frontend.common
 import kotlinx.js.jso
 import mui.material.styles.createTheme
 
-var fonts = listOf("Murecho", "sans-serif").toString().drop(1).dropLast(1)
+var fonts = listOf("Noto Sans JP", "Murecho", "sans-serif").toString().drop(1).dropLast(1)
 
 val SampleTheme = createTheme(
     jso{
