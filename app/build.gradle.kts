19fb562fd00fde2cdc4552dceaf613b094732880
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-10 22:02:06 +0900

    Ktorパッケージ追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,36 +17,27 @@ dependencies {
     // Use the Kotlin JS standard library.
     implementation(kotlin("stdlib-js"))
 
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-browser
+    // Ktor Core
+    implementation("io.ktor:ktor-client-core:1.6.7")
+
+    // Ktor Client
+    implementation("io.ktor:ktor-client-cio:1.6.7")
+
+    // Kotlin/JS React
     implementation("org.jetbrains.kotlin-wrappers:kotlin-browser:0.0.1-pre.312-kotlin-1.6.10")
-
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-react
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.312-kotlin-1.6.10")
-
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-react-dom
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.312-kotlin-1.6.10")
-
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-react-router-dom
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:6.2.1-pre.312-kotlin-1.6.10")
-
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-react-css
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-css:17.0.2-pre.312-kotlin-1.6.10")
-
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-extensions
     implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions:1.0.1-pre.312-kotlin-1.6.10")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-mui:5.5.0-pre.314-kotlin-1.6.10")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-mui-icons:5.5.0-pre.314-kotlin-1.6.10")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.7.1-pre.314-kotlin-1.6.10")
 
     // Kotlin Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-mui
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-mui:5.5.0-pre.314-kotlin-1.6.10")
-
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-mui-icons
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-mui-icons:5.5.0-pre.314-kotlin-1.6.10")
-
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-emotion
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.7.1-pre.314-kotlin-1.6.10")
-
+    // @emotion
     implementation(npm("@emotion/react", "11.8.1"))
     implementation(npm("@emotion/styled", "11.8.0"))
 }
