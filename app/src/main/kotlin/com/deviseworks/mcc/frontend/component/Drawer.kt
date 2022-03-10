37eee6c946ca2c17879917fbc714b7a4b0df2569
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-11 01:34:46 +0900

    各アイテムに応じたコンポーネントを表示するようにした

--- app/src/main/kotlin/com/deviseworks/mcc/frontend/component/Drawer.kt
+++ app/src/main/kotlin/com/deviseworks/mcc/frontend/component/Drawer.kt
@@ -2,6 +2,8 @@ package com.deviseworks.mcc.frontend.component
 
 import com.deviseworks.mcc.frontend.common.Area
 import com.deviseworks.mcc.frontend.common.Sizes
+import csstype.Color
+import csstype.None
 import mui.icons.material.ListAltOutlined
 import mui.icons.material.PeopleAltOutlined
 import mui.icons.material.SettingsOutlined
@@ -9,9 +11,16 @@ import mui.material.*
 import mui.system.sx
 import react.FC
 import react.Props
-import react.key
+import react.ReactNode
+import react.css.css
+import react.router.dom.NavLink
+import react.router.useLocation
+import react.useContext
 
 val DrawerPermanent = FC<Props>{
+    val tabs = useContext(TabsContext)
+    val lastPathname = useLocation().pathname.substringAfterLast("/")
+
     Box{
         sx{
             gridArea = Area.Drawer
@@ -27,24 +36,35 @@ val DrawerPermanent = FC<Props>{
                     width=Sizes.Drawer.Width
                 }
 
-                listOf("プレイヤー", "コンソール", "その他").forEachIndexed { index, s ->
-                    ListItemButton{
-                        key=s
-                        ListItemIcon{
-                            when(index){
-                                0 -> {
-                                    PeopleAltOutlined()
-                                }
-                                1 -> {
-                                    ListAltOutlined()
-                                }
-                                2 -> {
-                                    SettingsOutlined()
+                for((key, name) in tabs){
+                    NavLink{
+                        to = key
+
+                        css{
+                            textDecoration = None.none
+                            color = Color.currentcolor
+                        }
+
+                        ListItemButton{
+                            selected = key == lastPathname
+
+                            ListItemIcon{
+                                when(key){
+                                    "player" -> {
+                                        PeopleAltOutlined()
+                                    }
+                                    "console" -> {
+                                        ListAltOutlined()
+                                    }
+                                    "etc" -> {
+                                        SettingsOutlined()
+                                    }
                                 }
                             }
-                        }
-                        ListItemText{
-                            +s
+
+                            ListItemText{
+                                primary=ReactNode(name)
+                            }
                         }
                     }
                 }
