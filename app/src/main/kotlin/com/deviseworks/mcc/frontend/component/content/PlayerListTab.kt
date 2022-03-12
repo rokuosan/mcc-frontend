7925657caced6aa579a90b7e42b88b0e363c210b
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-12 23:18:37 +0900

    非同期でリストを取得できるようにした

--- app/src/main/kotlin/com/deviseworks/mcc/frontend/component/content/PlayerListTab.kt
+++ app/src/main/kotlin/com/deviseworks/mcc/frontend/component/content/PlayerListTab.kt
@@ -1,30 +1,64 @@
 package com.deviseworks.mcc.frontend.component.content
 
+import com.deviseworks.mcc.frontend.common.API
 import com.deviseworks.mcc.frontend.entity.Player
-import csstype.Display
-import csstype.FlexWrap
-import mui.material.Box
-import mui.system.sx
+import kotlinx.browser.window
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.await
+import kotlinx.coroutines.launch
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.json.Json
 import react.FC
 import react.Props
+import react.useEffectOnce
+import react.useState
 
 val PlayerListTab = FC<Props> {
-    val playerList = mutableListOf<Player>()
+    var playerList: List<Player> by useState(emptyList())
 
-    for(i in 1..100){
-        playerList.add(Player("MockUUID_$i", "MockUser_$i", "MockUser_$i", false, "ONLINE"))
-    }
-
-    Box{
-        sx{
-            display = Display.flex
-            flexWrap = FlexWrap.wrap
-        }
-
-        for(p in playerList){
-            PlayerListItem{
-                data = p
-            }
+    useEffectOnce {
+        mainScope.launch {
+            playerList = fetchPlayerList()
         }
     }
+
+    playerList.forEachIndexed{ _, p ->
+        PlayerListItem{
+            data = p
+        }
+    }
+}
+
+private val mainScope = MainScope()
+
+suspend fun fetchPlayerList(): List<Player>{
+    val response = window
+        .fetch(
+            API.PLAYER_LIST+"?offline=true"
+        )
+        .await()
+        .text()
+        .await()
+
+    return Json.decodeFromString(ListSerializer(Player.serializer()), response)
+
+//    var res: HttpResponse? = null
+//
+//    coroutineScope {
+//        launch{
+//            val job = launch{
+//                res = client.get(API.PLAYER_LIST){
+//                    headers{
+//                        append("Access-Control-Allow-Origin", "*")
+//                    }
+//                    parameter("offline", "true")
+//                }
+//            }
+//            job.join()
+//        }
+//    }
+//
+//    val json: String = res!!.receive()
+//
+//    return Json.decodeFromString(ListSerializer(Player.serializer()), json)
 }
