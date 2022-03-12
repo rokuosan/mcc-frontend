d5eac6280e9155e572bfec987ac7f7d68ac6fa17
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-12 21:25:21 +0900

    パッケージの調整

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,31 +12,34 @@ repositories {
     mavenCentral()
 }
 
-val ktorVersion = "2.0.0-beta-1"
+val ktorVersion = "1.6.7"
 
 dependencies {
-    // Use the Kotlin JS standard library.
+    // Kotlin
     implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-browser:0.0.1-pre.312-kotlin-1.6.10")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions:1.0.1-pre.312-kotlin-1.6.10")
 
     // Ktor
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-client-js:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.6.0")
 
-    // Kotlin/JS React
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-browser:0.0.1-pre.312-kotlin-1.6.10")
+
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.312-kotlin-1.6.10")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.312-kotlin-1.6.10")
+//    implementation(npm("react", "17.0.2"))
+//    implementation(npm("react-dom", "17.0.2"))
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:6.2.1-pre.312-kotlin-1.6.10")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-css:17.0.2-pre.312-kotlin-1.6.10")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions:1.0.1-pre.312-kotlin-1.6.10")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-mui:5.5.0-pre.314-kotlin-1.6.10")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-mui-icons:5.5.0-pre.314-kotlin-1.6.10")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.7.1-pre.314-kotlin-1.6.10")
 
-    // Kotlin Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
-
-    // @emotion
+    // NPM
     implementation(npm("@emotion/react", "11.8.1"))
     implementation(npm("@emotion/styled", "11.8.0"))
 }
@@ -50,7 +53,7 @@ kotlin {
 //                    port= 3000,
 //                    proxy = mutableMapOf("/api" to mapOf("target" to "http://localhost:8080")),
 //                    contentBase = mutableListOf("$buildDir/processedResources/js/main")
-//                )
+//                )-opt-in=kotlin.RequiresOptIn
 //            }
             commonWebpackConfig {
                 cssSupport.enabled = true
