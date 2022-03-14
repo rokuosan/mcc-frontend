83c578d0226b15b144398bdf3c50711f545d9383
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-14 23:52:10 +0900

    エンドポイントを追加

--- app/src/main/kotlin/com/deviseworks/mcc/frontend/common/API.kt
+++ app/src/main/kotlin/com/deviseworks/mcc/frontend/common/API.kt
@@ -4,4 +4,5 @@ object API {
     private const val API_BASE = "http://172.20.86.19:8080"
     const val PLAYER_LIST = "$API_BASE/api/player/list"
     const val SERVER_COMMAND = "$API_BASE/api/server/order/add"
+    const val SERVER_MEMORY = "$API_BASE/api/server/memory"
 }
