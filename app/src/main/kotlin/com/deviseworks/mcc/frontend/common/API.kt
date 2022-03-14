af5bf41c08d08506470531e2dcbc1636b5ffceaa
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-03-14 14:41:42 +0900

    APIのエンドポイントを新規登録

--- app/src/main/kotlin/com/deviseworks/mcc/frontend/common/API.kt
+++ app/src/main/kotlin/com/deviseworks/mcc/frontend/common/API.kt
@@ -1,5 +1,7 @@
 package com.deviseworks.mcc.frontend.common
 
 object API {
-    const val PLAYER_LIST = "http://172.27.1.231:8080/api/player/list"
+    private const val API_BASE = "http://172.27.1.231:8080"
+    const val PLAYER_LIST = "$API_BASE/api/player/list"
+    const val SERVER_COMMAND = "$API_BASE/api/server/order/add"
 }
